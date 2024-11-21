0b8fc46608665f5d2c14712e014c23247660531e
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-20 22:29:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 $pageTitle ="Home";
 include "view-header.php"; 
 ?>
-<div class="container text-center mt-4">
+<div class="container text-center mt-5">
     <h1>MIS 4013 Project</h1>
+        <form class="d-flex justify-content-center mt-4" role="search" action="search.php" method="GET" style="max-width: 600px; margin: auto;">
+        <input class="form-control me-2" type="search" name="q" placeholder="Search for Actors, Shows, or Platforms" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
 </div>
 <?php
 include "view-footer.php";
