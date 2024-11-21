401a4ffbb8eaa414614ded2f3e2d18087cd95bca
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-20 22:34:43 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -32,6 +32,10 @@
           <a class="nav-link" href="platform.php">Platforms</a>
         </li>
       </ul>
+      <form class="d-flex" role="search" action="search.php" method="GET">
+        <input class="form-control me-2" type="search" name="q" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
 </nav>
