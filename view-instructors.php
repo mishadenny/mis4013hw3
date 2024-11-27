afed885353ddab48659d76400ef8a0554b2388a4
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-26 21:57:20 -0600

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -1,5 +1,6 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-<div class="row">
+<head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head><div class="row">
   <div class="col">
     <h1>Actors</h1>
   </div>
@@ -40,7 +41,7 @@
             <form method="post" action="">
               <input type="hidden" name="iid" value="<?php echo $instructor['actor_id']; ?>">
               <input type="hidden" name="actionType" value="Delete">
-              <button type="submit" class="btn danger" onclick="return confirm('Are you sure?');">
+              <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
                 Delete
               </button>
             </form>
