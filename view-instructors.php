548f2407a700984846ef0280fde72699171bf317
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-26 18:24:23 -0600

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -3,72 +3,64 @@
     <h1>Actors</h1>
   </div>
   <div class="col-auto">
-  <?php
-  include "view-actors-newform.php"
-    ?>
+    <?php include "view-actors-newform.php"; ?>
   </div>
 </div>
+
 <div class="sort-by-button-group">
   <button data-sort-value="original-order" class="is-checked">Original Order</button>
   <button data-sort-value="name">Name</button>
   <button data-sort-value="age">Age</button>
 </div>
+
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Age</th>
-      <th></th>
-      <th></th>
-      <th></th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+        <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
-    <tbody>
-  <?php 
-    while ($instructor=$instructors->fetch_assoc()) {
-      ?>
-      <tr>
-        <td><?php echo $instructor['actor_id']; ?></td>
-        <td><?php echo $instructor['actor_name'];?></td>
-        <td><?php echo $instructor['age'];?></td>
-        
-        <td>
-      <?php
-       include "view-actors-editform.php"
-      ?>
-        </td>
-        <td>
-          <form method="post" action="">
-            <input type="hidden" name="iid" value="<?php echo $instructor['actor_id']; ?>">
-            <input type="hidden" name="actionType" value="Delete">
-            <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-              </svg>
-            </button>
-          </form>
-        </td>
-
-        <td><a href="courses-by-instructor.php?id=<?php echo $instructor['actor_id']; ?>">Shows</a></td>
-        
-      </tr>
-      <?php
-    }
-  ?>
+    <tbody class="grid">
+      <?php while ($instructor = $instructors->fetch_assoc()) { ?>
+        <tr class="element-item" data-name="<?php echo $instructor['actor_name']; ?>" data-age="<?php echo $instructor['age']; ?>">
+          <td><?php echo $instructor['actor_id']; ?></td>
+          <td class="name"><?php echo $instructor['actor_name']; ?></td>
+          <td class="age"><?php echo $instructor['age']; ?></td>
+          <td>
+            <?php include "view-actors-editform.php"; ?>
+          </td>
+          <td>
+            <form method="post" action="">
+              <input type="hidden" name="iid" value="<?php echo $instructor['actor_id']; ?>">
+              <input type="hidden" name="actionType" value="Delete">
+              <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                </svg>
+              </button>
+            </form>
+          </td>
+          <td><a href="courses-by-instructor.php?id=<?php echo $instructor['actor_id']; ?>">Shows</a></td>
+        </tr>
+      <?php } ?>
     </tbody>
   </table>
 </div>
+
 <script>
   // Initialize Isotope on the table body
   var $grid = $('.grid').isotope({
     itemSelector: '.element-item',
     layoutMode: 'vertical',
     getSortData: {
-      name: '[data-name]', // sort by name
-      age: '[data-age parseInt]' // sort by age
+      name: '[data-name]', // sort by name attribute
+      age: '[data-age parseInt]' // sort by age attribute as integer
     }
   });
 
