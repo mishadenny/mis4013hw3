fc6dbceed7cdf5124807bdde502139757d78621e
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-26 21:40:31 -0600

    Update view-instructors.php

--- view-instructors.php
+++ view-instructors.php
@@ -7,15 +7,22 @@
     th, td {
       text-align: left;
       padding: 12px;
-      border: 1px solid #ddd;
     }
 
     th {
       background-color: #f4f4f4;
     }
 
+    tbody tr:nth-child(odd) {
+      background-color: #f9f9f9; /* Light gray for odd rows */
+    }
+
+    tbody tr:nth-child(even) {
+      background-color: #ffffff; /* White for even rows */
+    }
+
     tbody tr:hover {
-      background-color: #dcdcdc; /* Highlight on hover */
+      background-color: #dcdcdc; /* Slight highlight on hover */
     }
   </style>
 <div class="row">
