458f11053690a56a4f73bbd6de613e4334d651da
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-30 23:30:57 -0600

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -2,7 +2,7 @@
 function selectCourses() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select show_id, show_title, genre
+        $stmt = $conn->prepare("select show_id, show_title, genre, link
 from `mis4013-hw3`.show");
         $stmt->execute();
         $result = $stmt->get_result();
