b8ad626404f1a8869cff67f6b9d41ebffb9e29e2
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-01 17:57:52 -0600

    Create view-show-input-list.php

---
+++ view-show-input-list.php
@@ -0,0 +1,13 @@
+<select class="form-select" id="cid" name="cid">
+<?php
+while ($showItem = $showList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedShow == $showItem['show_id']) {
+    $selText = "selected";
+  }
+?>
+  <option value="<?php echo $showItem['show_id']; ?>" <?=$selText?>><?php echo $showItem['show_name']; ?></option>
+<?php
+}
+?>
+</select>
