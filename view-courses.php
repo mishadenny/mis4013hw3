d67980daa71ef25a410d258f8de32ed12e110767
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-11-30 23:05:23 -0600

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -13,7 +13,6 @@
   ?>
     <div class="col-md-4">
       <div class="card" style="width: 18rem; margin-bottom: 20px;">
-        <!-- Optional image section -->
         <img src="path/to/image/<?php echo $course['show_id']; ?>.jpg" class="card-img-top" alt="Show Image">
         <div class="card-body">
           <h5 class="card-title"><?php echo $course['show_title']; ?></h5>
@@ -29,7 +28,7 @@
             </form>
             <form method="post" action="sections-by-course.php" style="display:inline;">
               <input type="hidden" name="cid" value="<?php echo $course['show_id']; ?>">
-              <button type="submit" class="btn btn-primary btn-sm">Sections</button>
+              <button type="submit" class="btn btn-primary btn-sm">Episodes</button>
             </form>
           </div>
         </div>
