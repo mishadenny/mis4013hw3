55b2ff6e7a4275867fc49ac6707ca4a463dd6471
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-03 22:39:26 -0600

    Update view-shows-editform.php

--- view-shows-editform.php
+++ view-shows-editform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editShowModal<?php echo $course['show_id'];?>">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editShowModal<?php echo $course['show_id']; ?>">
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
@@ -7,29 +7,44 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="editShowModal<?php echo $course['show_id'];?>" tabindex="-1" aria-labelledby="editShowModalLabel<?php echo $course['show_id'];?>" aria-hidden="true">
+<div class="modal fade" id="editShowModal<?php echo $course['show_id']; ?>" tabindex="-1" aria-labelledby="editShowModalLabel<?php echo $course['show_id']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editShowModalLabel<?php echo $course['show_id'];?>">Edit Show</h1>
+        <h1 class="modal-title fs-5" id="editShowModalLabel<?php echo $course['show_id']; ?>">Edit Show</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
           <div class="mb-3">
-            <label for="sTitle<?php echo $course['show_id'];?>" class="form-label">Show Title</label>
-            <input type="text" class="form-control" id="sTitle<?php echo $course['show_id'];?>" name="sTitle" value="<?php echo $course['show_title'];?>">
+            <label for="sTitle<?php echo $course['show_id']; ?>" class="form-label">Show Title</label>
+            <input type="text" class="form-control" id="sTitle<?php echo $course['show_id']; ?>" name="sTitle" value="<?php echo htmlspecialchars($course['show_title']); ?>">
           </div>
           <div class="mb-3">
-            <label for="sGenre<?php echo $course['show_id'];?>" class="form-label">Show Genre</label>
-            <input type="text" class="form-control" id="sGenre<?php echo $course['show_id'];?>" name="sGenre" value="<?php echo $course['genre'];?>">
+            <label for="sGenre<?php echo $course['show_id']; ?>" class="form-label">Show Genre</label>
+            <input type="text" class="form-control" id="sGenre<?php echo $course['show_id']; ?>" name="sGenre" value="<?php echo htmlspecialchars($course['genre']); ?>">
           </div>
           <div class="mb-3">
-            <label for="sLink<?php echo $course['show_id'];?>" class="form-label">Show Link</label>
-            <input type="text" class="form-control" id="sLink<?php echo $course['show_id'];?>" name="sLink" value="<?php echo $course['link'];?>">
+            <label for="sLink<?php echo $course['show_id']; ?>" class="form-label">Show Link</label>
+            <input type="text" class="form-control" id="sLink<?php echo $course['show_id']; ?>" name="sLink" value="<?php echo htmlspecialchars($course['link']); ?>">
           </div>
-              <input type="hidden" name="cid" value="<?php echo $course['show_id'];?>">
-              <input type="hidden" name="actionType" value="Edit">
+          <div class="mb-3">
+            <label for="platformID<?php echo $course['show_id']; ?>" class="form-label">Platform</label>
+            <select class="form-select" id="platformID<?php echo $course['show_id']; ?>" name="platformID">
+              <?php
+              while ($platformItem = $platformList->fetch_assoc()) {
+                  $selText = ($platformItem['platform_id'] == $course['platform_id']) ? "selected" : "";
+              ?>
+                <option value="<?php echo $platformItem['platform_id']; ?>" <?php echo $selText; ?>>
+                  <?php echo htmlspecialchars($platformItem['platform_name']); ?>
+                </option>
+              <?php
+              }
+              ?>
+            </select>
+          </div>
+          <input type="hidden" name="cid" value="<?php echo $course['show_id']; ?>">
+          <input type="hidden" name="actionType" value="Edit">
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
