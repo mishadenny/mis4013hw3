02188835d17dfdb3b843eaeb52d0dbfa62b1ec6b
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-03 23:36:02 -0600

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -13,12 +13,11 @@
   ?>
     <div class="col-md-4">
       <div class="card" style="width: 18rem; margin-bottom: 20px;">
-        <a href="<?php echo htmlspecialchars($course['link']); ?>" target="_blank">
-          <img src="<?php echo htmlspecialchars($course['link']); ?>" class="card-img-top" alt="<?php echo htmlspecialchars($course['show_title']); ?>">
-        </a>
+        <!-- Optional image section -->
+        <img src="path/to/image/<?php echo $course['show_id']; ?>.jpg" class="card-img-top" alt="Show Image">
         <div class="card-body">
-          <h5 class="card-title"><?php echo htmlspecialchars($course['show_title']); ?></h5>
-          <p class="card-text">Genre: <?php echo htmlspecialchars($course['genre']); ?></p>
+          <h5 class="card-title"><?php echo $course['show_title']; ?></h5>
+          <p class="card-text">Genre: <?php echo $course['genre']; ?></p>
           <div class="d-flex justify-content-between">
             <div>
               <?php include "view-shows-editform.php"; ?>
@@ -26,11 +25,11 @@
             <form method="post" action="" style="display:inline;">
               <input type="hidden" name="cid" value="<?php echo $course['show_id']; ?>">
               <input type="hidden" name="actionType" value="Delete">
-              <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">Delete</button>
+              <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?');">Delete</button>
             </form>
             <form method="post" action="sections-by-course.php" style="display:inline;">
               <input type="hidden" name="cid" value="<?php echo $course['show_id']; ?>">
-              <button type="submit" class="btn btn-info">Episodes</button>
+              <button type="submit" class="btn btn-primary btn-sm">Episodes</button>
             </form>
           </div>
         </div>
