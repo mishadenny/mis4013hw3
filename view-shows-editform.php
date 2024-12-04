40b1efa56b232bd1132624673de7a1b210e208f6
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-03 22:43:11 -0600

    Update view-shows-editform.php

--- view-shows-editform.php
+++ view-shows-editform.php
@@ -21,27 +21,20 @@
             <input type="text" class="form-control" id="sTitle<?php echo $course['show_id']; ?>" name="sTitle" value="<?php echo htmlspecialchars($course['show_title']); ?>">
           </div>
           <div class="mb-3">
+            <label for="platformid<?php echo $course['show_id']; ?>" class="form-label">Platform</label>
+            <?php
+                $showList = selectCoursesForInput();
+                $selectedShow = $course['show_id'];
+                include "view-show-input-list.php";
+            ?>
+        </div>
+          <div class="mb-3">
             <label for="sGenre<?php echo $course['show_id']; ?>" class="form-label">Show Genre</label>
             <input type="text" class="form-control" id="sGenre<?php echo $course['show_id']; ?>" name="sGenre" value="<?php echo htmlspecialchars($course['genre']); ?>">
           </div>
           <div class="mb-3">
             <label for="sLink<?php echo $course['show_id']; ?>" class="form-label">Show Link</label>
             <input type="text" class="form-control" id="sLink<?php echo $course['show_id']; ?>" name="sLink" value="<?php echo htmlspecialchars($course['link']); ?>">
-          </div>
-          <div class="mb-3">
-            <label for="platformID<?php echo $course['show_id']; ?>" class="form-label">Platform</label>
-            <select class="form-select" id="platformID<?php echo $course['show_id']; ?>" name="platformID">
-              <?php
-              while ($platformItem = $platformList->fetch_assoc()) {
-                  $selText = ($platformItem['platform_id'] == $course['platform_id']) ? "selected" : "";
-              ?>
-                <option value="<?php echo $platformItem['platform_id']; ?>" <?php echo $selText; ?>>
-                  <?php echo htmlspecialchars($platformItem['platform_name']); ?>
-                </option>
-              <?php
-              }
-              ?>
-            </select>
           </div>
           <input type="hidden" name="cid" value="<?php echo $course['show_id']; ?>">
           <input type="hidden" name="actionType" value="Edit">
