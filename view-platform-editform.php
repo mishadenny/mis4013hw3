0ebff6fb07594f003e189046c0c38989d13cdc77
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-05 23:17:19 -0600

    Update view-platform-editform.php

--- view-platform-editform.php
+++ view-platform-editform.php
@@ -1,4 +1,3 @@
-<!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editPlatformModal<?php echo $platform['platform_id'];?>">
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -6,7 +5,6 @@
   </svg>
 </button>
 
-<!-- Modal -->
 <div class="modal fade" id="editPlatformModal<?php echo $platform['platform_id'];?>" tabindex="-1" aria-labelledby="editPlatformModalLabel<?php echo $platform['platform_id'];?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
