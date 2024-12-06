d066d97bb1348236e708d8e7aa674067e0e19097
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-05 19:14:56 -0600

    Update view-platform.php

--- view-platform.php
+++ view-platform.php
@@ -1,51 +1,57 @@
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Headquarters</th>
-        <th>Show Count</th> <!-- Added Show Count column -->
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php 
-      while ($platform = $platformWithCounts->fetch_assoc()) { // Changed variable name
-      ?>
-        <tr>
-          <td><?php echo $platform['platform_id']; ?></td>
-          <td><?php echo $platform['platform_name']; ?></td>
-          <td><?php echo $platform['headquarters']; ?></td>
-          <td>
-            <?php include "view-platform-editform.php"; ?>
-          </td>
-          <td>
-            <form method="post" action="">
-              <input type="hidden" name="cid" value="<?php echo $platform['platform_id']; ?>">
-              <input type="hidden" name="actionType" value="Delete">
-              <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
-                Delete
-              </button>
-            </form>
-          </td>
-          <td>
-            <form method="post" action="shows-by-platform.php">
-              <input type="hidden" name="cid" value="<?php echo $platform['platform_id']; ?>">
-              <button type="submit" class="btn btn-info">Shows</button>
-            </form>
-          </td>
-        </tr>
-      <?php
-      }
-      ?>
-    </tbody>
-  </table>
-</div>
 <div class="row mt-5">
-  <div class="col">
+  <!-- Table Section -->
+  <div class="col-md-6">
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Headquarters</th>
+            <th>Show Count</th> <!-- Added Show Count column -->
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php 
+          while ($platform = $platformWithCounts->fetch_assoc()) { // Changed variable name
+          ?>
+            <tr>
+              <td><?php echo $platform['platform_id']; ?></td>
+              <td><?php echo $platform['platform_name']; ?></td>
+              <td><?php echo $platform['headquarters']; ?></td>
+              <td><?php echo $platform['show_count']; ?></td> <!-- Display Show Count -->
+              <td>
+                <?php include "view-platform-editform.php"; ?>
+              </td>
+              <td>
+                <form method="post" action="">
+                  <input type="hidden" name="cid" value="<?php echo $platform['platform_id']; ?>">
+                  <input type="hidden" name="actionType" value="Delete">
+                  <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
+                    Delete
+                  </button>
+                </form>
+              </td>
+              <td>
+                <form method="post" action="shows-by-platform.php">
+                  <input type="hidden" name="cid" value="<?php echo $platform['platform_id']; ?>">
+                  <button type="submit" class="btn btn-info">Shows</button>
+                </form>
+              </td>
+            </tr>
+          <?php
+          }
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <!-- Donut Chart Section -->
+  <div class="col-md-6">
     <h2>Platform Distribution by Show Count</h2>
     <canvas id="platformChart" style="width:100%;max-width:600px;"></canvas>
   </div>
