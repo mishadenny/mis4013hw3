9173500c26ac8c27e816cd1fe7066f050db2af25
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-05 22:39:17 -0600

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -11,7 +11,7 @@
   <?php 
     while ($instructor = $instructors->fetch_assoc()) {
   ?>
-    <div class="card">
+    <div class="card" style="background-color: #f5f5dc; margin: 10px;"> <!-- Beige background -->
       <div class="card-body">
         <h5 class="card-title"><?php echo $instructor['actor_name']; ?></h5>
         <ul class="list-group">
@@ -19,24 +19,24 @@
             $courses = selectCoursesByInstructors($instructor['actor_id']);
             while ($course = $courses->fetch_assoc()) {
           ?>
-            <li class="list-group-item">
-             Title: <?php echo $course['show_title']; ?> 
-             Title Episode: <?php echo $course['title_episode']; ?> 
-             Season: <?php echo $course['season_number']; ?> 
-             Episode <?php echo $course['episode_number']; ?>
+            <li class="list-group-item" style="background-color: #fff;"> <!-- White background for contrast -->
+              Title: <?php echo $course['show_title']; ?> 
+              Title Episode: <?php echo $course['title_episode']; ?> 
+              Season: <?php echo $course['season_number']; ?> 
+              Episode: <?php echo $course['episode_number']; ?>
             </li>
-           <li class="list-group-item">
-             <?php include "view-actors-with-shows-editform.php"; ?>
-           </li>
-           <li class="list-group-item">
+            <li class="list-group-item" style="background-color: #fff;">
+              <?php include "view-actors-with-shows-editform.php"; ?>
+            </li>
+            <li class="list-group-item" style="background-color: #fff;">
               <form method="post" action="">
-                  <input type="hidden" name="sid" value="<?php echo $course['episode_id']; ?>">
-                  <input type="hidden" name="actionType" value="Delete">
-                  <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
+                <input type="hidden" name="sid" value="<?php echo $course['episode_id']; ?>">
+                <input type="hidden" name="actionType" value="Delete">
+                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">
                   Delete
-              </button>
-            </form>
-           </li>
+                </button>
+              </form>
+            </li>
           <?php
             }
           ?>
