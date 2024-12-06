1e9af61c007015515c3f8e675a1911a2c9e486aa
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-05 23:09:28 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,9 +1,7 @@
 <?php
 function get_db_connection(){
-    // Create connection
     $conn = new mysqli('mis-4013-db-hw3.mysql.database.azure.com', 'mis4013', 'School10800!', 'mis4013-hw3');
     
-    // Check connection
     if ($conn->connect_error) {
       return false;
     }
