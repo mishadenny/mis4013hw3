88077358334ee7e65356b5ded217dcb7d79cff08
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-05 19:24:59 -0600

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -13,11 +13,11 @@ function selectCourses() {
     }
 }
 
-function InsertShow($sTitle, $sGenre, $sLink, $platformId, $simage) {
+function InsertShow($sTitle, $sGenre, $sLink, $platformId, $sImage) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `mis4013-hw3`.`show` (`show_title`, `genre`, `link`, `platform_id`, `image`) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssis", $sTitle, $sGenre, $sLink, $platformId, $simage);
+        $stmt->bind_param("sssis", $sTitle, $sGenre, $sLink, $platformId, $sImage);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -28,11 +28,11 @@ function InsertShow($sTitle, $sGenre, $sLink, $platformId, $simage) {
 }
 
 
-function UpdateShow($sTitle, $sGenre, $sLink, $platformId, $simage, $cid) {
+function UpdateShow($sTitle, $sGenre, $sLink, $platformId, $sImage, $cid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("UPDATE `mis4013-hw3`.`show` SET `show_title`=?, `genre`=?, `link`=?, `platform_id`=?, `image`=? WHERE show_id=?");
-        $stmt->bind_param("sssisi", $sTitle, $sGenre, $sLink, $platformId, $simage, $cid);
+        $stmt->bind_param("sssisi", $sTitle, $sGenre, $sLink, $platformId, $sImage, $cid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
