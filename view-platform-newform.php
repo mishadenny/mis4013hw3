d4b089dfd06f0a9e53bae763055dbc4589ddcd94
mishadenny <122938130+mishadenny@users.noreply.github.com>
2024-12-05 23:17:58 -0600

    Update view-platform-newform.php

--- view-platform-newform.php
+++ view-platform-newform.php
@@ -1,4 +1,3 @@
-<!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newPlatformModal">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -6,7 +5,6 @@
     </svg>
 </button>
 
-<!-- Modal -->
 <div class="modal fade" id="newPlatformModal" tabindex="-1" aria-labelledby="newPlatformModallabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
